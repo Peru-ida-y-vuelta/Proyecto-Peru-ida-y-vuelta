26cc16801ead44ee109d7e70db02739a7c7d86e4
Norabuena Rojas Diego <i201810564@cibertec.edu.pe>
2022-05-14 18:48:39 -0500

    Se agrega el proyecto

---
+++ src/test/java/org/cibertec/ProyectoIntegrador/WsProyectoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cibertec.ProyectoIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsProyectoIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
