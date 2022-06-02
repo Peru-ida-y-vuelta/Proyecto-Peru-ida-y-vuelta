3b97eb9909c7802760f60436d79f86ba38785074
yeah291 <82481330+yeah291@users.noreply.github.com>
2022-06-02 17:12:05 -0500

    Add files via upload

---
+++ src/main/java/org/cibertec/ProyectoIntegrador/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,48 @@
+package org.cibertec.ProyectoIntegrador.service.impl;
+
+import java.util.List;
+
+import org.cibertec.ProyectoIntegrador.entidades.Usuario;
+import org.cibertec.ProyectoIntegrador.repository.UsuarioRepositorio;
+import org.cibertec.ProyectoIntegrador.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+public class UsuarioServiceImpl implements UsuarioService{
+
+	
+	@Autowired
+	UsuarioRepositorio usuarioRepositorio;
+	
+	@Override
+	@Transactional(readOnly = true)
+	public List<Usuario> listarUsuario() {
+		// TODO Auto-generated method stub
+		return usuarioRepositorio.findAll();
+	}
+
+	@Override
+	public Usuario guardar(Usuario usu) {
+		// TODO Auto-generated method stub
+		return usuarioRepositorio.save(usu);
+	}
+
+	@Override
+	public Usuario actualizar(Usuario usu) {
+		// TODO Auto-generated method stub
+		return usuarioRepositorio.save(usu);
+	}
+
+	@Override
+	public void eliminar(int id) {
+		// TODO Auto-generated method stub
+		usuarioRepositorio.deleteById(id);
+	}
+
+	@Override
+	public Usuario buscarUsuario(int id) {
+		// TODO Auto-generated method stub
+		return usuarioRepositorio.findById(id).get();
+	}
+
+}
