f849312b30473192652a0397731f74d98ca87b7a
Diego Alonso Norabuena Rojas <i201810564@cibertec.edu.pe>
2022-06-17 20:10:15 -0500

    ultimos mensajes
    mensajes al hacer cambios en unidades

--- src/main/java/org/cibertec/ProyectoIntegrador/controller/UnidadController.java
+++ src/main/java/org/cibertec/ProyectoIntegrador/controller/UnidadController.java
@@ -1,7 +1,5 @@
 package org.cibertec.ProyectoIntegrador.controller;
 
-
-
 import java.util.List;
 
 import javax.validation.Valid;
@@ -18,7 +16,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/unidad")
@@ -28,21 +28,23 @@ public class UnidadController {
 	UnidadService uniService;
 	@Autowired
 	ChoferService choService;
-	 
+
 	@GetMapping("/")
 	public String listarUnidades(Model model) {
 		model.addAttribute("unidades", uniService.listarUnidad());
 		return "/unidad/listarunidad";
 	}
-	
+
 	@GetMapping("/crear")
 	public String crearUnidad(Model model) {
 		model.addAttribute("unidad", new Unidad());
 		model.addAttribute("choferes", choService.listarChofer());
 		return "/unidad/guardarunidad";
 	}
+
 	@PostMapping("/grabar")
-	public String grabarUnidad(@Valid @ModelAttribute Unidad unidad,BindingResult result,Model model) {
+	public String grabarUnidad(@Valid @ModelAttribute Unidad unidad, BindingResult result, Model model,
+			RedirectAttributes attribute) {
 		List<Chofer> choferes = choService.listarChofer();
 
 		if (result.hasErrors()) {
@@ -56,35 +58,50 @@ public class UnidadController {
 			return "/unidad/guardarunidad";
 		}
 		uniService.grabarUni(unidad);
+		attribute.addFlashAttribute("success", "Unidad Agregado");
 		return "redirect:/unidad/";
 	}
+
 	@GetMapping("/editar/{id}")
-	public String editaUnidad(@PathVariable("id") Integer idUnidad,Model model) {
-		
+	public String editaUnidad(@PathVariable("id") Integer idUnidad, Model model,RedirectAttributes attribute) {
+
+		if (idUnidad > 0) {
+				Unidad unidad = uniService.buscarUni(idUnidad);
+			if (unidad == null) {
+				System.out.println("ID no encontrado");
+				attribute.addFlashAttribute("error", "ID no encontrada");
+				return "redirect:/unidad/";
+			}
+		} else {
+			System.out.println("Error con el ID");
+			attribute.addFlashAttribute("error", "Error con la ID");
+			return "redirect:/unidad/";
+		}
+
 		List<Chofer> choferes = choService.listarChofer();
 		Unidad unidad = uniService.buscarUni(idUnidad);
 		model.addAttribute("unidad", unidad);
 		model.addAttribute("choferes", choferes);
-		
 		return "/unidad/editarunidad";
 	}
-	
+
 	@PostMapping("/actualizar")
-	public String actualizarUnidad(@Valid @ModelAttribute Unidad unidad,BindingResult result,Model model) {
+	public String actualizarUnidad(@Valid @ModelAttribute Unidad unidad, BindingResult result, Model model,
+			RedirectAttributes attribute) {
 		List<Chofer> choferes = choService.listarChofer();
 
 		if (result.hasErrors()) {
 			model.addAttribute("choferes", choferes);
 			return "/unidad/guardarunidad";
 		}
-		
 		uniService.actualizarUni(unidad);
+		attribute.addFlashAttribute("warning", "Cliente Actualizado");
 		return "redirect:/unidad/";
 	}
-	
+
 	@GetMapping("/eliminar/{id}")
-    public String choferEliminar(@PathVariable("id") Integer idUnidad) {
-    	 uniService.eliminarUni(idUnidad);
-    	return "redirect:/unidad/";
-    }
+	public String choferEliminar(@PathVariable("id") Integer idUnidad) {
+		uniService.eliminarUni(idUnidad);
+		return "redirect:/unidad/";
+	}
 }
